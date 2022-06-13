64cce5d083c45b04ced55d678ab14ef7b63a24c0
Afreen9689 <106980215+Afreen9689@users.noreply.github.com>
2022-06-13 21:20:16 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    azurerm = {
+      # Specify what version of the provider we are going to utilise
+      source = "hashicorp/azurerm"
+      version = ">= 2.4.1"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+      key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+data "azurerm_client_config" "current" {}
+# Create our Resource Group - Afreen-RG
+resource "azurerm_resource_group" "rg" {
+  name     = "Afreen-app01"
+  location = "UK South"
+}
+# Create our Virtual Network - Afreen-VNET
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Afreenvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+# Create our Subnet to hold our VM - Virtual Machines
+resource "azurerm_subnet" "sn" {
+  name                 = "VM"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = ["10.0.1.0/24"]
+}
+# Create our Azure Storage Account - afreensa
+resource "azurerm_storage_account" "afreensa" {
+  name                     = "afreensa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "Afreenrox"
+  }
+}
+# Create our vNIC for our VM and assign it to our Virtual Machines Subnet
+resource "azurerm_network_interface" "vmnic" {
+  name                = "Afreenvm01nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+# Create our Virtual Machine - Afreen-VM01
+resource "azurerm_virtual_machine" "Afreenvm01" {
+  name                  = "Afreenvm01"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vmnic.id]
+  vm_size               = "Standard_B2s"
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter-Server-Core-smalldisk"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "Afreenvm01os"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name      = "Afreenvm01"
+    admin_username     = "Afreen"
+    admin_password     = "Windows@123"
+  }
+  os_profile_windows_config {
+  }
+}
